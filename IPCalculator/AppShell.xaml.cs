daa8bb52300f627e7ee0bdd3dc0bfbd44c8038e6
Samuele Pecci <samuelepecci99@gmail.com>
2024-04-15 22:49:02 +0200

    Aggiungere i file di progetto.

---
+++ IPCalculator/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace IPCalculator
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ IPCalculator/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace IPCalculator
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
