3edbfa0806751412f2e938a1f5e70e07ef0dc9a6
Samuele Pecci <samuelepecci99@gmail.com>
2024-04-16 09:36:53 +0200

    Aggiunte le classi necessarie, rudimentale interfaccia

--- IPCalculator/MainPage.xaml.cs
+++ IPCalculator/MainPage.xaml.cs
@@ -2,23 +2,18 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void Calculate(object sender, EventArgs e)
         {
-            count++;
+            Configuration config = new(IPAddress.Text, SubnetMask.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            CalculateBtn.Text = config.GetIndirizzoRete();
+            //SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
 
