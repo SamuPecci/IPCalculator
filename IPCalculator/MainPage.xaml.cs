72a8d248fc709d6bdf7a6a79aa891c1fd24f25ed
Samuele Pecci <samuelepecci99@gmail.com>
2024-04-23 07:38:50 +0200

    Miglioramenti grafici, aggiunti first e last host, gestione errori

--- IPCalculator/MainPage.xaml.cs
+++ IPCalculator/MainPage.xaml.cs
@@ -9,11 +9,46 @@
 
         private void Calculate(object sender, EventArgs e)
         {
-            Configuration config = new(IPAddress.Text, SubnetMask.Text);
+            IPAddress.Placeholder = "Enter IP address";
+            IPAddress.PlaceholderColor = default;
+            SubnetMask.Placeholder = "Enter subnet mask";
+            SubnetMask.PlaceholderColor = default;
 
+            try
+            {
+                Configuration config = new(IPAddress.Text, SubnetMask.Text);
 
-            CalculateBtn.Text = config.GetIndirizzoRete();
-            //SemanticScreenReader.Announce(CounterBtn.Text);
+                NetworkAddress.Text = config.GetIndirizzoRete();
+                BroadcastAddress.Text = config.GetIndirizzoBroadcast();
+                FirstHost.Text = config.GetPrimoHost();
+                LastHost.Text = config.GetUltimoHost();
+            }
+            
+            catch (Exception ex)
+            {
+                switch (ex.Message)
+                {
+                    case "Indirizzo IP non valido":
+                        IPAddress.Text = "";
+                        IPAddress.Placeholder = "Incorrect IP address";
+                        IPAddress.PlaceholderColor = Colors.Red;
+                        break;
+
+                    case "Maschera di sottorete non valida":
+                        SubnetMask.Text = "";
+                        SubnetMask.Placeholder = "Incorrect subnet mask";
+                        SubnetMask.PlaceholderColor = Colors.Red;
+                        break;
+
+                    default:
+                        throw new Exception(ex.Message);
+                }
+
+                NetworkAddress.Text = "";
+                BroadcastAddress.Text = "";
+                FirstHost.Text = "";
+                LastHost.Text = "";
+            }
         }
     }
 
